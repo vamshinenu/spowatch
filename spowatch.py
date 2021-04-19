b511b979ed208bc22368f290afe94a852f86c7b5
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-04-19 19:51:56 +0530

    Update spowatch.py
    minor updates. use v 1.1 instead.

--- spowatch.py
+++ spowatch.py
@@ -4,7 +4,33 @@ import psutil
 import os
 import time
 import watch
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller, Listener
+import getpass
+
+paused = False
+user = getpass.getuser()
+spotify_path = "C:/Users/"+user+"/AppData/Roaming/Spotify/Spotify.exe"
+winapp_spotify_path = "C:/Users/"+user + \
+    "/AppData/Local/Microsoft/WindowsApps/SpotifyAB.SpotifyMusic_zpdnekdrzrea0/Spotify.exe"
+correct_spotify_path = ""
+
+
+def on_press(key):
+    global paused
+    w = win32gui
+    forwin = w.GetWindowText(w.GetForegroundWindow())
+    if forwin == "Spotify Free":
+        if key == Key.space:
+            paused = not paused
+
+
+def on_release(_):
+    pass
+
+
+def keyListener():
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
 
 
 def killThread():
@@ -13,7 +39,10 @@ def killThread():
     # pid = None
     for proc in psutil.process_iter():
         if process_name in proc.name():
-            proc.kill()
+            try:
+                proc.kill()
+            except psutil.NoSuchProcess:
+                pass
 
 
 def getPid():
@@ -25,58 +54,88 @@ def getPid():
     watch.pid_watch(pid, "w")
 
 
-def startApp():
+def play(init):
+    keyboard = Controller()
+    if init:
+        keyboard.press(Key.space)
+    else:
+        keyboard.press(Key.ctrl)
+        keyboard.press(Key.right)
+        keyboard.release(Key.right)
+        keyboard.release(Key.ctrl)
+
+
+def startApp(init):
     print("starting spotify...")
-    path = "C:/Users/va/AppData/Roaming/Spotify/Spotify.exe"
-    os.startfile(path)
+    # path = "C:/Users/va/AppData/Roaming/Spotify/Spotify.exe"
+    os.startfile(correct_spotify_path)
     watch.write_watch()
     getPid()
-    play()
+    play(init)
 
 
-def play():
-    keyboard = Controller()
-    keyboard.press(Key.ctrl)
-    keyboard.press(Key.right)
-    keyboard.release(Key.right)
-    keyboard.release(Key.ctrl)
+# def block_spowatch():
+#     w = win32gui
+#     forwin = w.GetWindowText(w.GetForegroundWindow())
+#     file = open("pausewatch.txt", "r")
+#     p = file.read()
+#     if p != 0:
+#         if forwin == "Spotify Free" and paused:
+#             file.close()
+#             watch.pause_watch(1)
+#             return False
 
 
-# TODO handle keyboard strokes
-pass
-
-
-def winEnumHandler(hwnd, ctx):
-
+def winEnumHandler(hwnd, _):
+    global paused
     if win32gui.IsWindowVisible(hwnd):
         wintext = win32gui.GetWindowText(hwnd)
-        tid, pid = win32process.GetWindowThreadProcessId(hwnd)
+        _, pid = win32process.GetWindowThreadProcessId(hwnd)
 
         li = watch.pid_watch("none", "r")
-        if wintext != None and wintext != "" and wintext != " ":
-            if wintext == "Spotify Free" or wintext == "Advertisement":
-                if watch.read_watch() < 1:
-                    print("Ad dectected or song paused...")
-                    killThread()
-                    time.sleep(0.5)
-                    startApp()
+        if not paused:
+            if wintext != None and wintext != "" and wintext != " ":
+                if wintext == "Spotify Free" or wintext == "Advertisement":
+                    if watch.read_watch() < 1:
+                        print("Ad dectected...")
+                        killThread()
+                        time.sleep(0.5)
+                        startApp(False)
+                    watch.write_watch()
 
-                watch.write_watch()
-
-            elif str(pid) in li:
-                if "Spotify" in wintext:
-                    pass
-                else:
-                    watch.init_watch()
-                    h = wintext.rstrip().lstrip()
-                    if watch.song_watch(h):
-                        print("playing: ", end=" ")
-                        print(h)
+                elif str(pid) in li:
+                    if "Spotify" in wintext:
+                        pass
+                    else:
+                        watch.init_watch()
+                        h = wintext.rstrip().lstrip()
+                        if watch.song_watch(h):
+                            print("playing: ", end=" ")
+                            print(h)
+        else:
+            print("song paused,spowatch is blocked")
 
 
 def main():
+    global correct_spotify_path
+    try:
+        os.startfile(spotify_path)
+    except FileNotFoundError:
+        try:
+            os.startfile(winapp_spotify_path)
+        except FileNotFoundError:
+            print("spotify not installed!")
+        else:
+            correct_spotify_path = winapp_spotify_path
+            pass
+        pass
+    else:
+        correct_spotify_path = spotify_path
+        pass
+    import threading
+    threading.Thread(target=keyListener)
     watch.init_watch()
-    startApp()
+    startApp(True)
     try:
         print("running spotify watcher...")
         while True:
@@ -85,13 +144,12 @@ def main():
     except KeyboardInterrupt:
         print("\n\nkeyboard interrupt, ending watch...\n")
         watch.end_watch()
+        killThread()
     except psutil.NoSuchProcess:
         print("process no longer exist, ending watch...")
+        killThread()
         watch.end_watch()
 
 
 if __name__ == "__main__":
     main()
-
-
-# todo: need to play after starting the app
